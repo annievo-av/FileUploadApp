c968a7824314505e80bb23383f33322167ee7858
Annie Vo <annievo.av@hotmail.com>
2019-06-29 13:44:01 -0500

    Ver 1

--- FileUploadApp/VendorInformation.Designer.cs
+++ FileUploadApp/VendorInformation.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.vendorTin.AutoSize = true;
             this.vendorTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.vendorTin.Location = new System.Drawing.Point(71, 141);
+            this.vendorTin.Location = new System.Drawing.Point(27, 59);
+            this.vendorTin.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.vendorTin.Name = "vendorTin";
-            this.vendorTin.Size = new System.Drawing.Size(224, 39);
+            this.vendorTin.Size = new System.Drawing.Size(94, 17);
             this.vendorTin.TabIndex = 0;
             this.vendorTin.Text = "Vendor\'s TIN:";
             this.vendorTin.Click += new System.EventHandler(this.VendorTin_Click);
@@ -57,9 +58,10 @@
             // 
             this.recipientTin.AutoSize = true;
             this.recipientTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.recipientTin.Location = new System.Drawing.Point(71, 241);
+            this.recipientTin.Location = new System.Drawing.Point(27, 101);
+            this.recipientTin.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.recipientTin.Name = "recipientTin";
-            this.recipientTin.Size = new System.Drawing.Size(257, 39);
+            this.recipientTin.Size = new System.Drawing.Size(107, 17);
             this.recipientTin.TabIndex = 1;
             this.recipientTin.Text = "Recipient\'s TIN:";
             this.recipientTin.Click += new System.EventHandler(this.RecipientTin_Click);
@@ -68,9 +70,10 @@
             // 
             this.incomeTax.AutoSize = true;
             this.incomeTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.incomeTax.Location = new System.Drawing.Point(71, 442);
+            this.incomeTax.Location = new System.Drawing.Point(27, 185);
+            this.incomeTax.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.incomeTax.Name = "incomeTax";
-            this.incomeTax.Size = new System.Drawing.Size(204, 39);
+            this.incomeTax.Size = new System.Drawing.Size(84, 17);
             this.incomeTax.TabIndex = 3;
             this.incomeTax.Text = "Income Tax:";
             // 
@@ -78,9 +81,10 @@
             // 
             this.recipientName.AutoSize = true;
             this.recipientName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.recipientName.Location = new System.Drawing.Point(71, 342);
+            this.recipientName.Location = new System.Drawing.Point(27, 143);
+            this.recipientName.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.recipientName.Name = "recipientName";
-            this.recipientName.Size = new System.Drawing.Size(292, 39);
+            this.recipientName.Size = new System.Drawing.Size(122, 17);
             this.recipientName.TabIndex = 2;
             this.recipientName.Text = "Recipient\'s Name:";
             this.recipientName.Click += new System.EventHandler(this.RecipientName_Click);
@@ -89,9 +93,10 @@
             // 
             this.texasResident.AutoSize = true;
             this.texasResident.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.texasResident.Location = new System.Drawing.Point(559, 538);
+            this.texasResident.Location = new System.Drawing.Point(210, 226);
+            this.texasResident.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.texasResident.Name = "texasResident";
-            this.texasResident.Size = new System.Drawing.Size(292, 43);
+            this.texasResident.Size = new System.Drawing.Size(125, 21);
             this.texasResident.TabIndex = 4;
             this.texasResident.Text = "Texas Resident";
             this.texasResident.UseVisualStyleBackColor = true;
@@ -105,45 +110,51 @@
             // vendorTinBox
             // 
             this.vendorTinBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.vendorTinBox.Location = new System.Drawing.Point(559, 141);
+            this.vendorTinBox.Location = new System.Drawing.Point(210, 59);
+            this.vendorTinBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.vendorTinBox.Name = "vendorTinBox";
             this.vendorTinBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.vendorTinBox.Size = new System.Drawing.Size(514, 45);
+            this.vendorTinBox.Size = new System.Drawing.Size(195, 22);
             this.vendorTinBox.TabIndex = 5;
             this.vendorTinBox.TabStop = false;
             // 
             // recipientTinBox
             // 
             this.recipientTinBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.recipientTinBox.Location = new System.Drawing.Point(559, 235);
+            this.recipientTinBox.Location = new System.Drawing.Point(210, 99);
+            this.recipientTinBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.recipientTinBox.Name = "recipientTinBox";
-            this.recipientTinBox.Size = new System.Drawing.Size(514, 45);
+            this.recipientTinBox.Size = new System.Drawing.Size(195, 22);
             this.recipientTinBox.TabIndex = 6;
             this.recipientTinBox.TabStop = false;
             // 
             // recipientNameBox
             // 
             this.recipientNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.recipientNameBox.Location = new System.Drawing.Point(559, 336);
+            this.recipientNameBox.Location = new System.Drawing.Point(210, 141);
+            this.recipientNameBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.recipientNameBox.Name = "recipientNameBox";
-            this.recipientNameBox.Size = new System.Drawing.Size(514, 45);
+            this.recipientNameBox.Size = new System.Drawing.Size(195, 22);
             this.recipientNameBox.TabIndex = 7;
             this.recipientNameBox.TabStop = false;
             // 
             // incomeTaxBox
             // 
             this.incomeTaxBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.incomeTaxBox.Location = new System.Drawing.Point(559, 436);
+            this.incomeTaxBox.Location = new System.Drawing.Point(210, 183);
+            this.incomeTaxBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.incomeTaxBox.Name = "incomeTaxBox";
-            this.incomeTaxBox.Size = new System.Drawing.Size(514, 45);
+            this.incomeTaxBox.Size = new System.Drawing.Size(195, 22);
             this.incomeTaxBox.TabIndex = 8;
             this.incomeTaxBox.TabStop = false;
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(932, 530);
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(350, 226);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(141, 51);
+            this.saveButton.Size = new System.Drawing.Size(53, 28);
             this.saveButton.TabIndex = 19;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -151,9 +162,9 @@
             // 
             // VendorInformation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1690, 812);
+            this.ClientSize = new System.Drawing.Size(578, 341);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.incomeTaxBox);
             this.Controls.Add(this.recipientNameBox);
@@ -164,6 +175,7 @@
             this.Controls.Add(this.recipientName);
             this.Controls.Add(this.recipientTin);
             this.Controls.Add(this.vendorTin);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "VendorInformation";
